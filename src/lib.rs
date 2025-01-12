dd051369eb1afefc874750acce5566fb6f6fa8ca
Michal Rostecki <vadorovsky@protonmail.com>
2025-01-12 16:11:40 +0100

    Add a basic Rust wrapper
    Instead of exposing the `c_char` arrays and `extern "C"` function
    pointers to the user of this library, hide all this stuff behind a
    wrapper.
    
    For now, the wrapper doesn't cover the function pointers, it's just
    enough for a minimal no-op example.
    
    Co-authored-by: Vaishali Thakkar <me.vaishalithakkar@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,188 +1,119 @@
 #![no_std]
-#![allow(non_camel_case_types)]
 
-use aya_ebpf::{
-    bindings::{cgroup, task_struct},
-    cty::{c_char, c_ulong},
+use core::{
+    ffi::{c_char, CStr},
+    slice,
 };
 
-#[repr(C)]
-#[derive(Debug, Copy, Clone)]
-pub struct cpumask {
-    _unused: [u8; 0],
+use thiserror::Error;
+
+pub mod bindings;
+use bindings::sched_ext_ops;
+
+#[derive(Debug, Error)]
+pub enum SchedExtOpsError {
+    #[error("name can have at most 127 characters, got {0}")]
+    NameTooLong(usize),
 }
 
-/// Argument container for [`cgroup_init`](sched_ext_ops::cgroup_init)
-/// operation.
-#[repr(C)]
-#[derive(Debug, Copy, Clone)]
-pub struct scx_cgroup_init_args {
-    pub weight: u32,
+#[repr(transparent)]
+pub struct SchedExtOps {
+    _inner: sched_ext_ops,
 }
 
-/// Argument container for [`cgroup_init`](sched_ext_ops::cpu_acquire)
-/// operation. It's currently empty, introduced as a placeholder.
-#[repr(C)]
-#[derive(Debug, Copy, Clone)]
-pub struct scx_cpu_acquire_args {}
+impl SchedExtOps {
+    pub fn new(name: &CStr) -> Result<Self, SchedExtOpsError> {
+        let name_len = name.count_bytes();
+        if name_len > 127 {
+            return Err(SchedExtOpsError::NameTooLong(name_len));
+        }
 
-/// Reason for SCX being preempted.
-#[repr(C)]
-#[derive(Debug, Copy, Clone)]
-pub enum scx_cpu_preempt_reason {
-    /// New task is being scheduled by `sched_class_rt`.
-    SCX_CPU_PREEMPT_RT = 0,
-    /// New task is being scheduled by `sched_class_dl`.
-    SCX_CPU_PREEMPT_DL = 1,
-    /// New task is being scheduled by `sched_class_stop`.
-    SCX_CPU_PREEMPT_STOP = 2,
-    /// Unknown reason for SCX being preempted.
-    SCX_CPU_PREEMPT_UNKNOWN = 3,
+        let mut c_name_buf: [c_char; 128] = [0; 128];
+
+        // `CStr` has methods like `to_bytes` and `to_bytes_with_nul`, both of
+        // them return `&[u8]`. We are interested in `&[c_char]` (which can be
+        // `&[i8]` on many architectures).
+        //
+        // Even though `CStr` uses `[c_char]` as an internal field, the only
+        // public method exposing `c_char` is `as_ptr`. So, unfortunately, the
+        // only option for us is to use it and build a slice from it.
+        //
+        // SAFETY: We are sure about the length of the string and we add one
+        // more byte for the NUL character.
+        let name_bytes: &[c_char] =
+            unsafe { slice::from_raw_parts(name.as_ptr(), name_len + 1) };
+        c_name_buf[..name_bytes.len()].copy_from_slice(name_bytes);
+
+        Ok(Self {
+            _inner: sched_ext_ops {
+                // TODO: Wrap functions.
+                select_cpu: None,
+                enqueue: None,
+                dequeue: None,
+                dispatch: None,
+                tick: None,
+                runnable: None,
+                running: None,
+                stopping: None,
+                quiescent: None,
+                yield_: None,
+                core_sched_before: None,
+                set_weight: None,
+                set_cpumask: None,
+                update_idle: None,
+                cpu_acquire: None,
+                cpu_release: None,
+                init_task: None,
+                exit_task: None,
+                enable: None,
+                disable: None,
+                dump: None,
+                dump_cpu: None,
+                dump_task: None,
+                cgroup_init: None,
+                cgroup_exit: None,
+                cgroup_prep_move: None,
+                cgroup_move: None,
+                cgroup_cancel_move: None,
+                cgroup_set_weight: None,
+                cpu_online: None,
+                cpu_offline: None,
+                init: None,
+                exit: None,
+
+                dispatch_max_batch: 0,
+                flags: 0,
+                timeout_ms: 0,
+                exit_dump_len: 0,
+                hotplug_seq: 0,
+
+                name: c_name_buf,
+            },
+        })
+    }
 }
 
-/// Argument container for [`cpu_release`](sched_ext_ops::cpu_release)
-/// operation.
-#[repr(C)]
-#[derive(Debug, Copy, Clone)]
-pub struct scx_cpu_release_args {
-    pub reason: scx_cpu_preempt_reason,
-    pub task: *mut task_struct,
-}
+#[cfg(test)]
+mod test {
+    use core::mem;
 
-/// Context provided to [`dump`](sched_ext_ops::dump),
-/// [`dump_cpu`](sched_ext_ops::dump_cpu) and
-/// [`dump_task`](sched_ext_ops::dump_task) operations.
-#[repr(C)]
-#[derive(Debug, Copy, Clone)]
-pub struct scx_dump_ctx {
-    pub kind: scx_exit_kind,
-    pub exit_code: i64,
-    pub reason: *const c_char,
-    pub at_ns: u64,
-    pub at_jiffies: u64,
-}
+    use super::*;
 
-/// Argument container for [`init_task`](sched_ext_ops::init_task) operation.
-#[repr(C)]
-#[derive(Debug, Copy, Clone)]
-pub struct scx_init_task_args {
-    pub fork: bool,
-    pub cgroup: *mut cgroup,
-}
+    #[test]
+    fn test_sched_ext_ops_minimal() {
+        let minimal = SchedExtOps::new(c"minimal").unwrap();
 
-/// Reason for exiting SCX.
-#[repr(C)]
-#[derive(Debug, Copy, Clone)]
-pub enum scx_exit_kind {
-    SCX_EXIT_NONE = 0,
-    /// Task is done.
-    SCX_EXIT_DONE = 1,
-    /// Unregistration initiated by the user-space.
-    SCX_EXIT_UNREG = 64,
-    /// Unregistration initiated by an eBPF program.
-    SCX_EXIT_UNREG_BPF = 65,
-    /// Unregistration initiated by the kernel.
-    SCX_EXIT_UNREG_KERN = 66,
-    /// Unregistration initiated by sysrq.
-    SCX_EXIT_SYSRQ = 67,
-    /// Runtime error.
-    SCX_EXIT_ERROR = 1024,
-    /// Error triggered by an eBPF program through `scx_bpf_error`.
-    SCX_EXIT_ERROR_BPF = 1025,
-    /// Watchdog detected stalled runnable tasks.
-    SCX_EXIT_ERROR_STALL = 1026,
-}
+        let inner_name: &[c_char] = &minimal._inner.name;
+        let inner_name: &[u8] = unsafe { mem::transmute::<&[c_char], &[u8]>(inner_name) };
+        let inner_name = CStr::from_bytes_until_nul(inner_name).unwrap();
 
-/// Information passed to [`exit`](sched_ext_ops::exit) to describe why the
-/// eBPF scheduler is being disabled.
-#[repr(C)]
-#[derive(Debug, Copy, Clone)]
-pub struct scx_exit_info {
-    /// Category of the exit reason.
-    pub kind: scx_exit_kind,
-    // Exit code.
-    pub exit_code: i64,
-    /// Textual representation of [`kind`](Self::kind) and
-    /// [`exit_code`](Self::exit_code).
-    pub reason: *const c_char,
-    /// Backtrace, if exiting due to an error.
-    pub bt: *mut c_ulong,
-    pub bt_len: u32,
-    /// Informational message.
-    pub msg: *mut c_char,
-    /// Debug dump.
-    pub dump: *mut c_char,
-}
+        assert_eq!(inner_name, c"minimal");
+    }
 
-/// Argument container for [`exit_task`](sched_ext_ops::exit_task) operation.
-#[repr(C)]
-#[derive(Debug, Copy, Clone)]
-pub struct scx_exit_task_args {
-    cancelled: bool,
-}
+    #[test]
+    fn test_sched_ext_ops_name_too_long() {
+        let res = SchedExtOps::new(c"hgsdlfgdsfgfdsgsdfagsdfgfdsgfdsgdfgdsfghfdshsdfgfdgfdgsdfgfdsgsdfgsdfhdgfhsfdgafdsgfdgsdfgfdgsdfgfdsgsdfdsgdfgsgdsfggdfsggfhgfdjhfgjgfhfdsrdgragsfdhgsdrgdthdfhgfdgfdgrdgsrgsdrtgdrgdfg");
 
-/// Operation table for eBPF scheduler implementation.
-#[repr(C)]
-#[derive(Debug, Copy, Clone)]
-pub struct sched_ext_ops {
-    /// Picks the target CPU for a task which is being woken up.
-    pub select_cpu:
-        Option<unsafe extern "C" fn(arg1: *mut task_struct, arg2: i32, arg3: u64) -> i32>,
-    /// Enqueues a task on the eBPF scheduler.
-    pub enqueue: Option<unsafe extern "C" fn(arg1: *mut task_struct, arg2: u64)>,
-    /// Removes a task from the eBPF scheduler.
-    pub dequeue: Option<unsafe extern "C" fn(arg1: *mut task_struct, arg2: u64)>,
-    /// Dispatches tasks from the eBPF scheduler and/or consumes DSQs.
-    pub dispatch: Option<unsafe extern "C" fn(arg1: i32, arg2: *mut task_struct)>,
-    /// Periodic tick.
-    pub tick: Option<unsafe extern "C" fn(arg1: *mut task_struct)>,
-    pub runnable: Option<unsafe extern "C" fn(arg1: *mut task_struct, arg2: u64)>,
-    pub running: Option<unsafe extern "C" fn(arg1: *mut task_struct)>,
-    pub stopping: Option<unsafe extern "C" fn(arg1: *mut task_struct, arg2: bool)>,
-    pub quiescent: Option<unsafe extern "C" fn(arg1: *mut task_struct, arg2: u64)>,
-    pub yield_:
-        Option<unsafe extern "C" fn(arg1: *mut task_struct, arg2: *mut task_struct) -> bool>,
-    pub core_sched_before:
-        Option<unsafe extern "C" fn(arg1: *mut task_struct, arg2: *mut task_struct) -> bool>,
-    pub set_weight: Option<unsafe extern "C" fn(arg1: *mut task_struct, arg2: u32)>,
-    pub set_cpumask: Option<unsafe extern "C" fn(arg1: *mut task_struct, arg2: *const cpumask)>,
-    pub update_idle: Option<unsafe extern "C" fn(arg1: i32, arg2: bool)>,
-    pub cpu_acquire: Option<unsafe extern "C" fn(arg1: i32, arg2: *mut scx_cpu_acquire_args)>,
-    pub cpu_release: Option<unsafe extern "C" fn(arg1: i32, arg2: *mut scx_cpu_release_args)>,
-    pub init_task:
-        Option<unsafe extern "C" fn(arg1: *mut task_struct, arg2: *mut scx_init_task_args) -> i32>,
-    pub exit_task:
-        Option<unsafe extern "C" fn(arg1: *mut task_struct, arg2: *mut scx_exit_task_args)>,
-    pub enable: Option<unsafe extern "C" fn(arg1: *mut task_struct)>,
-    pub disable: Option<unsafe extern "C" fn(arg1: *mut task_struct)>,
-    pub dump: Option<unsafe extern "C" fn(arg1: *mut scx_dump_ctx)>,
-    pub dump_cpu: Option<unsafe extern "C" fn(arg1: *mut scx_dump_ctx, arg2: i32, arg3: bool)>,
-    pub dump_task: Option<unsafe extern "C" fn(arg1: *mut scx_dump_ctx, arg2: *mut task_struct)>,
-    pub cgroup_init:
-        Option<unsafe extern "C" fn(arg1: *mut cgroup, arg2: *mut scx_cgroup_init_args) -> i32>,
-    pub cgroup_exit: Option<unsafe extern "C" fn(arg1: *mut cgroup)>,
-    pub cgroup_prep_move: Option<
-        unsafe extern "C" fn(arg1: *mut task_struct, arg2: *mut cgroup, arg3: *mut cgroup) -> i32,
-    >,
-    pub cgroup_move:
-        Option<unsafe extern "C" fn(arg1: *mut task_struct, arg2: *mut cgroup, arg3: *mut cgroup)>,
-    pub cgroup_cancel_move:
-        Option<unsafe extern "C" fn(arg1: *mut task_struct, arg2: *mut cgroup, arg3: *mut cgroup)>,
-    pub cgroup_set_weight: Option<unsafe extern "C" fn(arg1: *mut cgroup, arg2: u32)>,
-    pub cpu_online: Option<unsafe extern "C" fn(arg1: i32)>,
-    pub cpu_offline: Option<unsafe extern "C" fn(arg1: i32)>,
-    /// Initialized the eBPF scheduler.
-    pub init: Option<unsafe extern "C" fn() -> i32>,
-    /// Cleans up after the eBPF scheduler.
-    pub exit: Option<unsafe extern "C" fn(arg1: *mut scx_exit_info)>,
-    pub dispatch_max_batch: u32,
-    pub flags: u64,
-    /// The maximum amount of time, in milliseconds, that a runnable task
-    /// should be able to wait before being scheduled. The maximum timeout
-    /// cannot exceed the default timeout of 30 seconds.
-    pub timeout_ms: u32,
-    pub exit_dump_len: u32,
-    pub hotplug_seq: u64,
-    /// eBPF scheduler's name.
-    pub name: [c_char; 128usize],
+        assert!(matches!(res, Err(SchedExtOpsError::NameTooLong(_))));
+    }
 }
